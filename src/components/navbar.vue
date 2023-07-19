c17bef69676f0a88e70ada959ccf7228991cb568
rahat-mohiuddin <mmohin.rahat@gmail.com>
2023-07-20 00:57:05 +0600

    fix dropdown navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,63 +1,73 @@
 <template>
-    <nav class="el-nav-menu">
-        <ul>
-          <li v-for="item in navItems" :key="item.id">
-            <router-link class="nav-link" :to="item.link" @mouseover="toggleDropdown(item.id)" @mouseleave="closeDropdown(item.id)">{{ item.label }}</router-link>
-            <ul class="el-dropdown" v-if="dropdowns[item.id]" @mouseover="cancelCloseDropdown(item.id)" @mouseleave="closeDropdown(item.id)">
-              <li v-for="subItem in item.subItems" :key="subItem.id">
-                <router-link class="nav-link" :to="subItem.link">{{ subItem.label }}</router-link>
-              </li>
-            </ul>
+  <nav class="el-nav-menu">
+    <ul>
+      <li v-for="(item, index) in navItems" :key="index" @mouseover="openDropdown(index)" @mouseleave="closeDropdown(index)">
+        <router-link class="nav-link" :to="item.url">{{ item.label }}</router-link>
+        <ul class="el-dropdown" v-if="item.dropdownItems && item.dropdownItems.length" :class="{ 'show-dropdown': item.showDropdown }">
+          <li v-for="(dropdownItem, dropdownIndex) in item.dropdownItems" :key="dropdownIndex">
+            <router-link class="nav-link" :to="dropdownItem.url">{{ dropdownItem.label }}</router-link>
           </li>
         </ul>
-      </nav>
+      </li>
+    </ul>
+  </nav>
 </template>
+
 <script>
-import { ref } from 'vue';
 export default {
-    // const siteUrl = import.meta.env.VITE_BUILD_ADDRESS;
-    setup() {
-        const navItems = [
-            { id: 1, label: 'Home', link: '/' },
-            { id: 2, label: 'Profile', link: '/profile' },
-            { id: 3, label: 'About', link: '/about' },
-            { id: 4, label: 'Services', link: '/services',
-              subItems: [
-                { id: 11, label: 'Course', link: '/course' },
-                { id: 12, label: 'Tutorial', link: '/tutorial' },
-                { id: 13, label: 'Application', link: '/application' },
-                { id: 14, label: 'Blog', link: '/blog' }
-              ]
+  data() {
+    return {
+      navItems: [
+        {
+          label: 'Home',
+          url: '/'
+        },
+        {
+          label: 'About',
+          url: '/about',
+          dropdownItems: []
+        },
+        {
+          label: 'Our Team',
+          url: '/team',
+          dropdownItems: []
+        },
+        {
+          label: 'Services',
+          url: '/services',
+          dropdownItems: [
+            {
+              label: 'Cource',
+              url: '/cource'
             },
-            { id: 5, label: 'Contact', link: '/contact' }
-        ];
-        const dropdowns = {};
-        navItems.forEach(item => {
-          dropdowns[item.id] = false;
-        });
-
-        const toggleDropdown = (itemId) => {
-          dropdowns[itemId] = !dropdowns[itemId];
-          console.log('warking')
-        };
-
-        const closeDropdown = (itemId) => {
-          dropdowns[itemId] = false;
-          console.log('warking too')
-        };
-
-        const cancelCloseDropdown = (itemId) => {
-          console.log('warking three')
-          clearTimeout(dropdownTimeouts[itemId]);
-        };
-
-        return {
-          navItems,
-          dropdowns,
-          toggleDropdown,
-          closeDropdown,
-          cancelCloseDropdown,
-        };
+            {
+              label: 'Tutorial',
+              url: '/tutorial'
+            },
+            {
+              label: 'Application',
+              url: '/application'
+            },
+            {
+              label: 'Blog',
+              url: '/blog'
+            }
+          ]
+        },
+        {
+          label: 'Contact',
+          url: '/contact'
+        }
+      ]
+    };
+  },
+  methods: {
+    openDropdown(index) {
+      this.navItems[index].showDropdown = true;
     },
-}
+    closeDropdown(index) {
+      this.navItems[index].showDropdown = false;
+    }
+  }
+};
 </script>
